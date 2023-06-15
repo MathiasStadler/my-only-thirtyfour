262666f306ed40a3763c05ee499a856665696e27
dudoof <email@example.com>
2023-06-15 14:03:46 +0200

    maximize_window()

--- examples/tokio_finviz.rs
+++ examples/tokio_finviz.rs
@@ -24,21 +24,29 @@ async fn run() -> color_eyre::Result<()> {
     // it much easier to locate where the error occurred.
     color_eyre::install()?;
 
-    let caps = DesiredCapabilities::chrome();
+    let caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
+
+    
     //let caps = DesiredCapabilities::firefox();
-    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    let driver: WebDriver = WebDriver::new("http://localhost:9515", caps).await?;
+
+    driver.maximize_window().await?;
+
     // Navigate to https://wikipedia.org.
     driver.goto("https://finviz.com").await?;
-    let elem_form = driver.find(By::Id("search-form")).await?;
+
+   // driver.fullscreen_window().await?;
+
+    let elem_form: WebElement = driver.find(By::Id("search-form")).await?;
 
     // Find element from element.
-    let elem_text = elem_form.find(By::Id("searchInput")).await?;
+    let elem_text: WebElement = elem_form.find(By::Id("searchInput")).await?;
 
     // Type in the search terms.
     elem_text.send_keys("selenium").await?;
 
     // Click the search button.
-    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+    let elem_button: WebElement = elem_form.find(By::Css("button[type='submit']")).await?;
     elem_button.click().await?;
 
     // Look for header to implicitly wait for the page to load.
