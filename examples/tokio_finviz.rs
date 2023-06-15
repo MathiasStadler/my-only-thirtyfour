6f06b785e819b9fc3900072eed9ee2bce1ded4a2
dudoof <email@example.com>
2023-06-15 14:49:22 +0200

    select exchange

--- examples/tokio_finviz.rs
+++ examples/tokio_finviz.rs
@@ -7,6 +7,7 @@
 //!     cargo run --example tokio_async
 
 use thirtyfour::prelude::*;
+use tokio::time::*;
 
 fn main() -> color_eyre::Result<()> {
     let rt: tokio::runtime::Runtime = tokio::runtime::Builder::new_current_thread().enable_all().build()?;
@@ -24,36 +25,60 @@ async fn run() -> color_eyre::Result<()> {
     // it much easier to locate where the error occurred.
     color_eyre::install()?;
 
-    let caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
-
+    let mut caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
+    caps.add_chrome_arg("--enable-automation")?;
     
     //let caps = DesiredCapabilities::firefox();
     let driver: WebDriver = WebDriver::new("http://localhost:9515", caps).await?;
 
-    driver.maximize_window().await?;
+    //driver.maximize_window().await?;
 
-    // Navigate to https://wikipedia.org.
+    // Navigate to https://finviz.com
     driver.goto("https://finviz.com").await?;
 
-   // driver.fullscreen_window().await?;
+    //wait 5 second
+    //from here
+    //https://dev.to/stevepryde/using-selenium-with-rust-aca
+    tokio::time::sleep(Duration::from_secs(5)).await;
 
-    let elem_form: WebElement = driver.find(By::Id("search-form")).await?;
+    println!("Title = {}", driver.title().await?);
+
+    // just a test ok
+    // driver.fullscreen_window().await?;
+
+    let elem_form: WebElement = driver.find(By::XPath("/html/body/div[1]/div/div/div/div[2]/div/button[3]")).await?;
+    elem_form.click().await?;
+
+    // click on screener
+    // xpath
+    // /html/body/table[2]/tbody/tr/td/table/tbody/tr/td[3]/a
+    let elem_screener: WebElement = driver.find(By::XPath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[3]/a")).await?;
+    elem_screener.click().await?;
+
+    //wait for screener
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    //select exchange
+    let exchange_nyse_xpath="/html/body/div[3]/table/tbody/tr[3]/td/div/form/table/tbody/tr[1]/td[2]/select/option[4]";
+    let elem_exchange_nyse_xpath: WebElement = driver.find(By::XPath(exchange_nyse_xpath)).await?;
+    elem_exchange_nyse_xpath.click().await?;
 
     // Find element from element.
-    let elem_text: WebElement = elem_form.find(By::Id("searchInput")).await?;
+    //let elem_text: WebElement = elem_form.find(By::Id("searchInput")).await?;
 
     // Type in the search terms.
-    elem_text.send_keys("selenium").await?;
+    //elem_text.send_keys("selenium").await?;
 
     // Click the search button.
-    let elem_button: WebElement = elem_form.find(By::Css("button[type='submit']")).await?;
-    elem_button.click().await?;
+    //let elem_button: WebElement = elem_form.find(By::Css("button[type='submit']")).await?;
+    //elem_button.click().await?;
 
     // Look for header to implicitly wait for the page to load.
-    driver.find(By::ClassName("firstHeading")).await?;
-    assert_eq!(driver.title().await?, "Selenium – Wikipedia");
+    //driver.find(By::ClassName("firstHeading")).await?;
+    //assert_eq!(driver.title().await?, "Selenium – Wikipedia");
 
     // Always explicitly close the browser. There are no async destructors.
+    
     driver.quit().await?;
 
     Ok(())
